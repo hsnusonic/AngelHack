08a5aae75fcac5597aa40c3387d62e564d606409
Man-Wei Huang <manwei12721@gmail.com>
2017-07-29 15:34:00 +0800

    Delete unnecessary files.

--- frontend/src/components/page/place/utils.js
+++ frontend/src/components/page/place/utils.js
@@ -1,8 +1,6 @@
 import moment from "moment";
 import uuid from "uuid";
 
-let weekdays = ["", "一", "二", "三", "四", "五", "六", "日"];
-
 export function formatDate(obj) {
 	var result = [];
 	/*get array of milliseconds*/
@@ -18,78 +16,4 @@ export function formatDate(obj) {
 		});
 	});
 	return result;
-}
-
-export function getUniqueVersion(arr) {
-	var tmpArr = [];
-	/*get version array*/
-	arr.forEach(function(obj) {
-			tmpArr = tmpArr.concat(obj.movie_version);
-		}
-	);
-	/*get unique version array*/
-	var result = Array.from(new Set(tmpArr));
-	return result;
-}
-
-export function getObjByDate(arr, date) {
-	var tmpArr = arr.find(function(obj) {
-		return (date === obj.date);
-	});
-	return tmpArr;
-}
-
-export function getListObjByVersion(arr, version) {
-	var tmpArr = arr.filter(function(obj) {
-		return (version === obj.movie_version);
-	});
-	return tmpArr;
-}
-
-export function getMovieNames(arr) {
-	var tmpArr = [];
-	arr.forEach(function(obj) {
-		tmpArr = [
-			...tmpArr,
-			[obj.movie_id,
-			obj.movie_title]
-		];
-	});
-	return tmpArr;
-}
-
-export function getGradeText(num) {
-	var text;
-	switch (num) {
-		case 0:
-			text = "輔15級";
-			break;
-		case 1:
-			text = "普遍級";
-			break;
-		case 2:
-			text = "保護級";
-			break;
-		case 3:
-			text = "輔12級";
-			break
-		case 4:
-			text = "限制級";
-			break;
-		default:
-			text = "";
-	}
-	return text;
-}
-
-export function getMovieStaff(arr) {
-	var obj = {};
-	console.log("arr: " + arr);
-	obj.cast = arr.filter(function(obj) {
-		return (obj.staff_type === "2");
-	});
-	obj.director = arr.filter(function(obj) {
-		return (obj.staff_type === "1");
-	});
-	return obj;
 }
